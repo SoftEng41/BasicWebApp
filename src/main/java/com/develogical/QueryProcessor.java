ad925ea84964e1e30e7693d68dd3b03db2984cf5
Hang Li Li <hang1997li@gmail.com>
2020-01-21 11:41:44 +0000

    Fix

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -26,6 +26,11 @@ public class QueryProcessor {
                 String[] s1 = s[1].split("multiplied");
                 return "" + (Integer.parseInt(s1[0].replaceAll("\\s+", "")) * Integer
                         .parseInt(s1[1].replaceAll("\\s+", "")));
+            } else if (query.toLowerCase().contains("minus")) {
+                String[] s = query.toLowerCase().split("is");
+                String[] s1 = s[1].split("minus");
+                return "" + (Integer.parseInt(s1[0].replaceAll("\\s+", "")) - Integer
+                        .parseInt(s1[1].replaceAll("\\s+", "")));
             } else {
                 return "";
             }
