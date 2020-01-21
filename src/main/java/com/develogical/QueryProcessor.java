e220cb4128cbd323fc4d89bcae39470354f68f27
Hang Li Li <hang1997li@gmail.com>
2020-01-21 11:18:46 +0000

    Fixed the nullpointer

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,13 +3,17 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.toLowerCase().contains("hang")) {
-            return "Hang Li Li is a Computing student at Imperial College London";
+        try {
+            if (query.toLowerCase().contains("shakespeare")) {
+                return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+                        "English poet, playwright, and actor, widely regarded as the greatest " +
+                        "writer in the English language and the world's pre-eminent dramatist.";
+            } else if (query.toLowerCase().contains("hang")) {
+                return "Hang Li Li is a Computing student at Imperial College London";
+            }
+            return "";
+        } catch (NullPointerException e) {
+            return "";
         }
-        return "";
     }
 }
